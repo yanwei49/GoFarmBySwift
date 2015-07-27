23d0a957d3c7b9f0e74fab9d3e83e226395bbb7a
yanwei <yanwei@example.com>
2015-07-27 14:55:42 +0800

    delete
    删除无用代码

--- GoFarmBySwift/MessageViewController.swift
+++ GoFarmBySwift/MessageViewController.swift
@@ -104,11 +104,5 @@ class MessageViewController: RCConversationListViewController, RCIMReceiveMessag
         self.navigationController?.showViewController(chatVC, sender: nil)
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
 
 }
